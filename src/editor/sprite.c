358ff11b2cd1d31b15ffbf6f33eed9b982382e4a
3Marz <omar490v@gmail.com>
2024-12-02 01:00:34 +0300

    Missed the files :p in the last commit

---
+++ src/editor/sprite.c
@@ -0,0 +1,53 @@
+
+#include "sprite.h"
+#include "../api.h"
+#include <raylib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+SpriteEditor *sprite_editor_init() {
+	SpriteEditor *e = malloc(sizeof(SpriteEditor));
+	e->selected = 0;
+	e->zoom = 1;
+	return e;
+}
+
+void sprite_editor_run(SpriteEditor *e, Ram *ram) {
+	RectF(ram, 192/2, 0, 192/2, 128, 15);
+	// Right Side
+	Rectangle canvas = {104, 16, 8*8, 8*8};
+	RectF(ram, canvas.x, canvas.y, canvas.width, canvas.height, 0);
+
+	// Left Side
+	int xoff = 0;
+	int yoff = 8;
+	
+	Mouse(ram);
+	int mx = Peek(ram, RAM_MOUSE_START);
+	int my = Peek(ram, RAM_MOUSE_START+1);
+	int mbtn = Peek(ram, RAM_MOUSE_START+2);
+
+
+	if (mx < 192/2 && mbtn == 1) {
+		e->selected = ((mx/8) + (my/8)*12)-12; 
+	}
+
+	for (int i = 0; i < 180; i++) {
+		Spr(ram, i, xoff, yoff, 0, 1, 1, 1);
+
+		if (e->selected == i)
+			Rect(ram, xoff-1, yoff-1, 10, 10, 2);
+		/*Rect(ram, xoff, yoff, 8, 8, 2);*/
+		xoff += 8;
+		if (xoff >= 8*12) {
+			xoff = 0;
+			yoff += 8;
+		}
+	}
+}
+
+void sprite_editor_close(SpriteEditor *e) {
+	free(e);
+}
+
---
+++ src/editor/sprite.h
@@ -0,0 +1,15 @@
+#ifndef SPRITE_H
+#define SPRITE_H
+
+#include "../ram.h"
+
+typedef struct SpriteEditor {
+	int selected;
+	int zoom;
+} SpriteEditor;
+
+SpriteEditor *sprite_editor_init();
+void sprite_editor_run(SpriteEditor *e, Ram *ram);
+void sprite_editor_close(SpriteEditor *e);
+
+#endif
